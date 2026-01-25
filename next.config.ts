1f0d2812d66234e0f2732c80ec18e5ed5531ae9e
raul <bjon.net.90@gmail.com>
2026-01-25 14:51:56 +0000

    cambia la imagen de fondo por un enlace funcional que ponga esta imagen

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
