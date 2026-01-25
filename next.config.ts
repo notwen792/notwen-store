4509764cc500f17b7a1747349354ff19a9373eea
raul <bjon.net.90@gmail.com>
2026-01-25 14:31:52 +0000

    PON QUE LA IMAGEN QUE APAREZCA SEA POR ENLACE Y PON ESTE ENLACE PARA QUE

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
